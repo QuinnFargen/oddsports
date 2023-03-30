19e63a201e6a606d413ed6f274caf242ae80a3cb
Quinn Fargen <quinnfargen@gmail.com>
2023-03-30 13:19:43 -0500

    finished cleanUP

--- Odds/APIoptions.py
+++ Odds/APIoptions.py
@@ -30,3 +30,63 @@ for r in range(len(result_list)):
 
 
 
+###################################
+
+import statsapi
+
+teams = statsapi.get(endpoint='teams',params={'sportId': 1})
+teams
+
+
+import pymongo
+import config
+import requests
+import json
+
+myclient = pymongo.MongoClient(config.mango)
+mydb = myclient["Stats"]
+mycol = mydb["MLB"]
+
+mycol.insert_one(teams)
+
+
+from Storage.OddsMongo import mongo_insertone
+
+mongo_insertone('Stats', 'MLB', teams)
+teams
+
+
+
+sched = statsapi.schedule(start_date='07/01/2023',end_date='07/31/2023')
+
+sched[0]
+
+
+
+##########################
+
+
+
+from nba_api.stats.endpoints import playercareerstats
+
+# Nikola Jokić
+career = playercareerstats.PlayerCareerStats(player_id='203999') 
+
+# pandas data frames (optional: pip install pandas)
+career.get_data_frames()[0]
+
+# json
+career.get_json()
+
+# dictionary
+career.get_dict()
+
+
+from nba_api.stats.endpoints import leaguegamelog
+
+
+leaguegl = leaguegamelog.LeagueGameLog(date_from_nullable='2023-03-20') 
+l = leaguegl.get_dict()
+leaguegl.get_json()
+
+list(l)[1]
--- Odds/OddsMLB.py
+++ Odds/OddsMLB.py
@@ -5,37 +5,11 @@ import requests
 import json
 
 from Storage.OddsMongo import mongo_insertone
+from Odds.OddsAPI import oddsApi_get
 
 
 sport = 'baseball_mlb'
-regions = 'us'
-oddsFormat = 'decimal'
-markets = 'h2h,spreads,totals'
-dateFormat = 'iso'
-url = "https://api.apilayer.com/odds/sports/"+sport+"/odds?regions="+regions+"&oddsFormat="+oddsFormat+"&markets="+markets+"&dateFormat="+dateFormat+""
 
-payload = {}
-headers= {  "apikey": config.odds_API_key}
+mlb = oddsApi_get(sport)
 
-response = requests.request("GET", url, headers=headers, data = payload)
-
-status_code = response.status_code
-# result = response.text
-# print(result)
-result_list = json.loads(response.text)
-len(result_list)
-# result_list[0]
-# response.json()
-
-################
-# MongoDB
-
-
-mongo_insertone('Stats', 'MLB', result_list, loop=1)
-
-# myclient = pymongo.MongoClient(config.mango)
-# mydb = myclient["Odds"]
-# mycol = mydb["MLB"]
-
-# for r in range(len(result_list)):
-#     x = mycol.insert_one(result_list[r])
+mongo_insertone('Odds', 'MLB', mlb, loop=1)
